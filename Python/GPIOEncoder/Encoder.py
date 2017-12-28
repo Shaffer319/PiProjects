4d4c099e929067fb7dff190cdec4ced80505d0ca
Shaffer319 <michaelshaffer319@gmail.com>
2017-12-28 17:46:51 -0500

    Added test as text.py and added a poll to figure out if it is missing steps

--- Python/GPIOEncoder/Encoder.py
+++ Python/GPIOEncoder/Encoder.py
@@ -1,13 +1,38 @@
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 
+class GPIO(object):
+    HIGH = 1
+    LOW = 0
+    IN = 0
+    OUT = 1
+    INOUT = 2
+    PUD_UP = 1
+    BOTH = 2
+    BOARD = 1
+    @staticmethod
+    def input(pin):
+        return GPIO.HIGH
+    @staticmethod
+    def setup(pins, mode, pull_up_down=PUD_UP):
+        pass
+    @staticmethod
+    def setmode(value):
+        pass
+    @staticmethod
+    def getmode():
+        return None
+    @staticmethod
+    def add_event_detect(pin, mode, callback):
+        pass
+        
 class Encoder(object):
     def __init__(self, pin_a, pin_b):
         self.ticks = 0
         self.last_change = 0
 
         self.no_change = 0
-        self.a = False
-        self.b = False
+        self.a = 0
+        self.b = 0
         self.setup= False
         if GPIO.getmode() is None:
             GPIO.setmode(GPIO.BOARD)
@@ -19,16 +44,17 @@ class Encoder(object):
         GPIO.setup(pins, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         
         # attach interrupt
-        GPIO.add_event_detect(self.pin_a, GPIO.BOTH, callback=self.change)
-        GPIO.add_event_detect(self.pin_b, GPIO.BOTH, callback=self.change)
+        GPIO.add_event_detect(self.pin_a, GPIO.BOTH, callback=self.change_and_poll)
+        GPIO.add_event_detect(self.pin_b, GPIO.BOTH, callback=self.change_and_poll)
         
     def change(self, channel):
+        # Seems to work in test but on on pi
         #a = self.a
         #b = self.b
         if not self.setup:
             self.setup = True
-            self.a = GPIO.input(self.pin_a)is GPIO.HIGH
-            self.b = GPIO.input(self.pin_b)is GPIO.HIGH
+            a = GPIO.input(self.pin_a)is GPIO.HIGH
+            b = GPIO.input(self.pin_b)is GPIO.HIGH
             return
 
         if(channel == self.pin_a):
@@ -53,6 +79,43 @@ class Encoder(object):
                 print("LEFT")
         print("{},a:{},b:{}".format(self.ticks, self.a, self.b))
 
+    def change_and_poll(self, channel):
+        a = GPIO.input(self.pin_a)
+        b = GPIO.input(self.pin_b)
+        
+        c = a << 1 | b
+        d = self.a << 1 | self.b
+               
+        if d == 0b00 and c == 0b11 or 
+            d == 0b01 and c == 0b10 or
+            d == 0b10 and c == 0b01 or 
+            d == 0b11 and c == 0b00:
+            print("Invalid state change")
+            return;
+            
+        if d == 0b00 and c == 0b10 or 
+            d == 0b01 and c == 0b00 or
+            d == 0b10 and c == 0b11 or 
+            d == 0b11 and c == 0b01:
+            print("Right")
+            return;
+            
+        if d == 0b00 and c == 0b01 or 
+            d == 0b01 and c == 0b11 or
+            d == 0b10 and c == 0b00 or 
+            d == 0b11 and c == 0b10:
+            print("Left")
+            return;
+            
+        if d == 0b00 and c == 0b00 or 
+            d == 0b01 and c == 0b01 or
+            d == 0b10 and c == 0b10 or 
+            d == 0b11 and c == 0b11:            
+            print("None")
+            return;
+            
+        print("Woops")
+        
     def pin_change(self, channel):
         # print("pin_change {}".format(channel))
         val_a = GPIO.input(self.pin_a)
---
+++ Python/GPIOEncoder/text.py
@@ -0,0 +1,9 @@
+import Encoder
+
+e = Encoder.Encoder(1,2)
+
+
+
+for i in range(10):
+    e.change(2)
+    e.change(1)
