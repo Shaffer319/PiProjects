707c2905ea418f352433ecb7d82e27173d93431d
Shaffer319 <michaelshaffer319@gmail.com>
2017-12-28 12:17:13 -0500

    Changed encoder to print when pins detect changes

--- Python/GPIOEncoder/Encoder.py
+++ Python/GPIOEncoder/Encoder.py
@@ -2,15 +2,28 @@ import RPi.GPIO as GPIO
 
 class Encoder(object):
     def __init__(self, pin_a, pin_b):
-    
+        self.ticks = 0
+            
         if GPIO.getmode() is None:
             GPIO.setmode(GPIO.BOARD)
             
         self.pin_a = pin_a
         self.pin_b = pin_b
-
+        
         pins = [self.pin_a, self.pin_b]
         GPIO.setup(pins, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    
+        
+        # attach interrupt
+        GPIO.add_event_detect(self.pin_a, GPIO.CHANGE, callback=self.pin_a_change, bouncetime=1)
+        GPIO.add_event_detect(self.pin_b, GPIO.CHANGE, callback=self.pin_b_change, bouncetime=1)
+        
+    def pin_a_change(self):
+        print("pin_a_change")
+        pass
+        
+    def pin_b_change(self):
+        print("pin_b_change")
+        pass
+        
     def read(self):
         return [GPIO.input(self.pin_a), GPIO.input(self.pin_b)]
--- Python/GPIOEncoder/__main__.py
+++ Python/GPIOEncoder/__main__.py
@@ -4,23 +4,27 @@ import time
 
 
 def main():
-    print("GPIOEncoder/__main__.py is running main()")
-    GPIO.setmode(GPIO.BOARD) 
+    print("Entering GPIOEncoder/__main__.py:")
     
-    GPIO.setup(15, GPIO.OUT)
-    GPIO.setup(16, GPIO.OUT)
-    encoder = Encoder(11,13)
-    # GPIO.setup([15,16], GPIO.OUT)
-    
-    # for i in range(0, 10):
-    #    time.sleep(1)
-    for i in range(0, 1000): #while(True):
-        value = encoder.read()
-        GPIO.output(15, value[0])
-        GPIO.output(16, value[1])
-        time.sleep(.01)
-   
+    try :
+        GPIO.setmode(GPIO.BOARD) 
+        
+        GPIO.setup(15, GPIO.OUT)
+        GPIO.setup(16, GPIO.OUT)
+        encoder = Encoder(11,13)
+        # GPIO.setup([15,16], GPIO.OUT)
+        
+        # for i in range(0, 10):
+        #    time.sleep(1)
+        for i in range(0, 1000): #while(True):
+            value = encoder.read()
+            GPIO.output(15, value[0])
+            GPIO.output(16, value[1])
+            time.sleep(.01)
+        
+    except KeyboardInterrupt:
+        GPIO.cleanup()
     GPIO.cleanup()
-
+    print("Exiting GPIOEncoder/__main__.py")
 if __name__ == "__main__":
     main()
